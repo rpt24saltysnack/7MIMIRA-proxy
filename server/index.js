44d8d35a052d132330881c431a82e1ba59e5310d
7MIMIRA <7mimira@gmail.com>
2020-12-14 23:36:31 -0500

    Change urls to point to deployed services

--- server/index.js
+++ server/index.js
@@ -10,8 +10,8 @@ app.use(express.static('public'));
 app.get('/bundles', (req, res) => {
   async function retrieveBundles() {
     try {
-      let colorAndSizeSelectionComponent = await axios.get('http://localhost:3001/bundle.js');
-      let summaryComponent = await axios.get('http://localhost:3002/bundle.js');
+      let colorAndSizeSelectionComponent = await axios.get('http://3.18.69.132:3001/bundle.js');
+      let summaryComponent = await axios.get('http://54.241.116.3:3002/bundle.js');
       res.send(`${summaryComponent.data}${colorAndSizeSelectionComponent.data}`);
     } catch (error) {
       console.error(error);
@@ -24,7 +24,7 @@ app.get('/bundles', (req, res) => {
 //color & size selection routes
 app.get('/shoes/:shoeId/colors', (req, res) => {
   let { shoeId } = req.params;
-  axios.get(`http://localhost:3001/shoes/${shoeId}/colors`)
+  axios.get(`http://3.18.69.132:3001/shoes/${shoeId}/colors`)
   .then(colors => {
     res.send(colors.data);
   })
@@ -36,7 +36,7 @@ app.get('/shoes/:shoeId/colors', (req, res) => {
 
 app.get('/shoes/:shoeId/sizes', (req, res) => {
   let { shoeId } = req.params;
-  axios.get(`http://localhost:3001/shoes/${shoeId}/sizes`)
+  axios.get(`http://3.18.69.132:3001/shoes/${shoeId}/sizes`)
   .then(sizes => {
     res.send(sizes.data);
   })
@@ -48,7 +48,7 @@ app.get('/shoes/:shoeId/sizes', (req, res) => {
 
 app.get('/shoes/:shoeId/colors/:colorId/quantities', (req, res) => {
   let { shoeId, colorId } = req.params;
-  axios.get(`http://localhost:3001/shoes/${shoeId}/colors/${colorId}/quantities`)
+  axios.get(`http://3.18.69.132:3001/shoes/${shoeId}/colors/${colorId}/quantities`)
   .then(quantities => {
     res.send(quantities.data);
   })
@@ -61,7 +61,7 @@ app.get('/shoes/:shoeId/colors/:colorId/quantities', (req, res) => {
 //product info routes
 app.get('/products/:shoeId/summary', (req, res) => {
   let { shoeId } = req.params;
-  axios.get(`http://localhost:3002/products/${shoeId}/summary`)
+  axios.get(`http://54.241.116.3:3002/products/${shoeId}/summary`)
   .then(summary => {
     res.send(summary.data);
   })
