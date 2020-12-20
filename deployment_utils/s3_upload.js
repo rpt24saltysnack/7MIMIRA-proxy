8fead92f6ed75ad08463ef256385dbec97fe5016
7MIMIRA <7mimira@gmail.com>
2020-12-20 02:45:58 -0500

    Add s3 utility files and refactor html
    index.html will now run a testBundle.js file located in an AWS S3 bucket

---
+++ deployment_utils/s3_upload.js
@@ -0,0 +1,34 @@
+/*
+* Run file using: node s3_upload.js <BUCKET_NAME> <FILE_NAME>
+*/
+
+// Load the AWS SDK for Node.js and other modules
+var AWS = require('aws-sdk');
+var fs = require('fs');
+var path = require('path');
+// Set the region
+AWS.config.update({region: 'REGION'});
+
+// Create S3 service object
+s3 = new AWS.S3({apiVersion: '2006-03-01'});
+
+// call S3 to retrieve upload file to specified bucket
+var uploadParams = {Bucket: process.argv[2], Key: '', Body: ''};
+var file = process.argv[3];
+
+// Configure the file stream and obtain the upload parameters
+var fileStream = fs.createReadStream(file);
+fileStream.on('error', function(err) {
+  console.log('File Error', err);
+});
+uploadParams.Body = fileStream;
+uploadParams.Key = path.basename(file);
+
+// call S3 to retrieve upload file to specified bucket
+s3.upload (uploadParams, function (err, data) {
+  if (err) {
+    console.log("Error", err);
+  } if (data) {
+    console.log("Upload Success", data.Location);
+  }
+});
