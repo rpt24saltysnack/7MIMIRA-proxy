ff0097dcedd1f20953e8f839aef54d3792b88963
7MIMIRA <7mimira@gmail.com>
2021-01-13 01:58:09 -0500

    Add product image component

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
     "../somebirdsColorAndSizeSelection/client/index.jsx",
     "../somebirds-product-accordion/client/index.jsx",
     "../somebirdsReviews/client/index.jsx",
-    "../somebirdsNavBar/client/index.jsx"
+    "../somebirdsNavBar/client/index.jsx",
+    "../somebirds-product-images/client/index.jsx"
   ],
   module: {
     rules: [
